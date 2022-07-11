d43224924de5b1f50b5f2a2cddacf9901cee05a6
Martin Craig <martin.craig@nottingham.ac.uk>
2022-07-11 13:55:15 +0100

    More options to specify exactly where subject QC is to be found

--- squat/main.py
+++ squat/main.py
@@ -29,22 +29,27 @@ import sys
 
 from ._version import __version__
 
-def get_subjects(fname):
-    if not fname:
-        raise ValueError(f"A subject list (--subjects) must be provided when using --extract")
-
-    try:
-        with open(fname) as fp:
-            return [l.strip() for l in fp.readlines()]
-    except IOError as exc:
-        raise ValueError(f"Failed to read subject directories from {fname}: {exc}")
+def get_subjects(subjdir, fname):
+    if fname:       
+        try:
+            with open(fname) as fp:
+                return [l.strip() for l in fp.readlines()]
+        except IOError as exc:
+            raise ValueError(f"Failed to read subject IDs from {fname}: {exc}")
+    else:
+        try:
+            return os.listdir(subjdir)
+        except IOError as exc:
+            raise ValueError(f"Failed to find any subject directories in {subjdir}: {exc}")
 
 def main():
     """
     Tool for generating QC reports for single subjects and groups
     """
     parser = argparse.ArgumentParser('Generalised Study-wise QUality Assessment Tool', add_help=True)
-    parser.add_argument('--subjects', help='text file containing a list of single-subject QC output folders')
+    parser.add_argument('--subjdir', default=".", help='Path to directory containing single-subject output')
+    parser.add_argument('--subjects', help='Path to text file containing a list of subject IDs. If not specified will use all subdirectories of --subjdir')
+    parser.add_argument('--qcpath', default="qc.json", help='Path to QC output file relative to subject directory')
     parser.add_argument('--extract', action="store_true", default=False, help="Extract data from single-subject QC output into group data file")
     parser.add_argument('--group-data', help="JSON file containing previously extracted group QC data")
     parser.add_argument('--group-report', action="store_true", default=False, help="Generate group report")
@@ -77,37 +82,43 @@ def main():
         raise ValueError(f"Output directory {args.output} already exists - remove or specify a different name")
     os.makedirs(args.output, exist_ok=True)
 
-    if args.extract:
-        subject_list = get_subjects(args.subjects)
+    if args.extract or args.subject_reports:
+        subjids = get_subjects(args.subjdir, args.subjects)
+        subjqcdata = []
+        for subjid in subjids:
+            subjdir = os.path.join(args.subjdir, subjid)
+            try:
+                with open(os.path.join(subjdir, args.qcpath)) as qc_file:
+                    subjqcdata.append(json.load(qc_file))
+            except IOError as exc:
+                raise ValueError(f"Could not read subject data for subject {subjid}: {exc}")
 
+    if args.extract:
         sys.stdout.write('Generating group data...')
-        group_data = data.main(os.path.join(args.output, "group_data.json"), 'w', subject_list)
+        sys.stdout.flush()
+        group_data = data.main(os.path.join(args.output, "group_data.json"), 'w', subjqcdata)
         sys.stdout.write('DONE\n')
     else:
         group_data = data.main(args.group_data, 'r')
 
     if args.group_report:
         sys.stdout.write('Generating group QC report...')
+        sys.stdout.flush()
         pdf = PdfPages(os.path.join(args.output, "group_report.pdf"))
-        refs.main(pdf)
+        #refs.main(pdf)
         report.main(pdf, report_def, group_data)
         pdf.close()
         sys.stdout.write('DONE\n')
     
     if args.subject_reports:
         sys.stdout.write('Generating subject QC reports...')
-        subject_list = get_subjects(args.subjects)
-        for subjdir in subject_list:
-            subjid = os.path.basename(subjdir)
-            try:
-                with open(os.path.join(subjdir, 'qc.json')) as qc_file:
-                    subject_data = json.load(qc_file)
-            except IOError as exc:
-                raise ValueError(f"Could not read subject data for subject {subjdir}: {exc}")
-            
+        sys.stdout.flush()
+        for subjid, subject_data in zip(subjids, subjqcdata):
+            sys.stdout.write(subjid + " ")
+            sys.stdout.flush()
             pdf = PdfPages(os.path.join(args.output, f"{subjid}_report.pdf"))
-            refs.main(pdf)
-            report.main(pdf, report_def, group_data, subject_data)
+            #refs.main(pdf)
+            report.main(pdf, report_def, group_data, subject_data, subjid)
             pdf.close()
         sys.stdout.write('DONE\n')
 
