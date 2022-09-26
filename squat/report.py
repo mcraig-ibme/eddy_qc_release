64efc8ad0c365a976fd7c29b13c2a398462b9c98
Martin Craig <martin.craig@nottingham.ac.uk>
2022-09-26 13:41:18 +0100

    Handle missing row labels

--- squat/report.py
+++ squat/report.py
@@ -201,7 +201,7 @@ class Report():
                             LOG.warn(f"Row labels specified to come from {row_labels} but this data item was not found")
                             row_labels = None
 
-                    if len(row_labels) != len(data_values):
+                    if row_labels is not None and len(row_labels) != len(data_values):
                         LOG.warn(f"Number of row labels {row_labels} does not match number of data items {len(data_values)}")
                         row_labels = None
 
